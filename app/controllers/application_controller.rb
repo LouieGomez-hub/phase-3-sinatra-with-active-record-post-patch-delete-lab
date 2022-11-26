532350800cc473618fb9d1ced493d618083260d4
louie_gomez <kinggomez027.lg@gmail.com>
2022-11-25 19:17:10 -0500

    PATCH/DELETE all tests passing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,19 @@ class ApplicationController < Sinatra::Base
     )
     baked_good.to_json
   end
+
+  patch '/bakeries/:id' do
+    bakery = Bakery.find(params[:id])
+    bakery.update(
+      name: params[:name]
+    )
+    bakery.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    baked_good = BakedGood.find(params[:id])
+    baked_good.destroy
+    baked_good.to_json
+  end
+
 end
